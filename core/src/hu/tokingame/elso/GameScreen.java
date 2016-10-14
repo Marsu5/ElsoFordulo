98955b107f16655408541759128b4a878944edd3
Marsu5 <marcell.schuh@gmail.com>
2016-10-14 16:30:32 +0200

    Load img + GameScreen, TeglaActor

---
+++ core/src/hu/tokingame/elso/GameScreen.java
@@ -0,0 +1,14 @@
+package hu.tokingame.elso;
+
+import com.badlogic.gdx.Game;
+
+/**
+ * Created by M on 10/14/2016.
+ */
+
+public class GameScreen extends MyScreen {
+    public GameScreen(Game game) {
+        super(game);
+    }
+
+}
