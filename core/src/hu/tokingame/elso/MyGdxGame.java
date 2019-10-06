48e1172228a05eaa7b533d3e29353708ed50baa7
Majzer <bence@mozer.hu>
2019-10-06 17:30:44 +0200

    Update MyGdxGame.java

--- core/src/hu/tokingame/elso/MyGdxGame.java
+++ core/src/hu/tokingame/elso/MyGdxGame.java
@@ -11,6 +11,10 @@ credits (készítők)
  */
 
 public class MyGdxGame extends Game {
+	
+	public static final int WORLD_HEIGHT = 720;
+	public static final int WORLD_WIDTH = 1280;
+	
 	@Override
 	public void create () {
 		Assets.prepare();
